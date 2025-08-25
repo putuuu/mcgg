3f9281d7e04bbc7c5fb704a6706c8619d88d694f
Putuuu <90541574+putuuu@users.noreply.github.com>
2025-08-25 14:07:34 +0700

    new bg

--- src/app/components/commander/CommanderCard.tsx
+++ src/app/components/commander/CommanderCard.tsx
@@ -2,6 +2,7 @@ import { Commander } from "../../data/commanders";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./CommanderCard.module.css";
+import { lazy } from "react";
 
 export default function CommanderCard({ commander }: { commander: Commander }) {
   return (
